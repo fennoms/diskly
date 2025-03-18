c097d5dd72255d4ebc288cd817f06d33de63bbbf
Fennom Schalkwijk <schalkwijkfennom@gmail.com>
2025-03-18 21:23:32 +0100

    ✨ Create code to run bot

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from diskly!")
+import os
 
+import discord
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+    client.run(os.getenv("DISCORD_TOKEN"))
